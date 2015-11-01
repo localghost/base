b04d7cea4f31b4b0662ac114e033b4b827a9a7fd
localghost <zkostrzewa@gmail.com>
2015-11-01 15:08:50 +0100

    Make basic logger tests automatic.
    Closes #10

--- tests/base/log.cc
+++ tests/base/log.cc
@@ -2,16 +2,34 @@
 
 #include <base/log.h>
 
+#include <sstream>
+
 BOOST_AUTO_TEST_SUITE(TSBaseLog)
+
+namespace detail {
+std::ostream& operator/(std::ostream& stream, std::stringbuf & buffer)
+{
+   stream.rdbuf(&buffer);
+   return stream;
+}
+}
 
 BOOST_AUTO_TEST_CASE(TCBaseLogDebug)
 {
-    LOG_DEBUG << "Test debug log";
+   using namespace detail;
+
+   std::stringstream ss;
+   LOG_DEBUG / *ss.rdbuf() << "Test debug log";
+   BOOST_CHECK_EQUAL(ss.str(), "Test debug log");
 }
 
 BOOST_AUTO_TEST_CASE(TCBaseLogInfo)
 {
-    LOG_INFO << "Test info log " << 42;
+   using namespace detail;
+
+   std::stringstream ss;
+   LOG_INFO / *ss.rdbuf() << "Test info log " << 42;
+   BOOST_CHECK_EQUAL(ss.str(), "Test info log 42");
 }
 
 //BOOST_AUTO_TEST_CASE(TCEncodeLongString)
@@ -21,7 +39,7 @@ BOOST_AUTO_TEST_CASE(TCBaseLogInfo)
 //
 //BOOST_AUTO_TEST_CASE(TCEncodeUtf8String)
 //{
-//    const char* const data = "zażółć";
+//    const char* const data = "za��������";
 //    const char* const expected = "emHFvMOzxYLEhw==";
 //    BOOST_CHECK_EQUAL(base64::encode(data), expected);
 //}
